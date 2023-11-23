2678cb094fb89d88d8f81b1e6510621f3f6357b0
Satendra Sahani <satendrasahani19@gmail.com>
2023-11-23 14:20:58 +0530

    student result

--- controller/student/result/result.js
+++ controller/student/result/result.js
@@ -1,54 +1,89 @@
-
 const User = require("../../../models/User");
 const Module = require("../../../models/Module");
 const Course = require("../../../models/Course");
 const { default: mongoose } = require("mongoose");
+const asyncHandler = require("../../../middleware/async");
 exports.saveResult = async (req, callBack) => {
-    const allQuestion = await Module.find({ courseId: req.courseId }).select(
-      "questions"
-    );
-    const courseDetail = await Course.findOne({ _id: req.courseId });
-    const oneQuestionMark =
-      parseFloat(courseDetail.maxMark) / req.noOfAllQuestion;
-    let obtainedScore = 0;
-    for await (let answer of req.answer) {
-      for await (let qstn of allQuestion) {
-        if (
-          qstn.questions.some(
-            (item) =>
-              item.question == answer.questionName && item.answer == answer.answer
-          )
-        ) {
-          obtainedScore = oneQuestionMark + obtainedScore;
-        }
+  const allQuestion = await Module.find({ courseId: req.courseId }).select(
+    "questions"
+  );
+  const courseDetail = await Course.findOne({ _id: req.courseId });
+  const oneQuestionMark =
+    parseFloat(courseDetail.maxMark) / req.noOfAllQuestion;
+  let obtainedScore = 0;
+  for await (let answer of req.answer) {
+    for await (let qstn of allQuestion) {
+      if (
+        qstn.questions.some(
+          (item) =>
+            item.question == answer.questionName && item.answer == answer.answer
+        )
+      ) {
+        obtainedScore = oneQuestionMark + obtainedScore;
       }
     }
-  
-    const user = await User.findById(req.user._id);
-    // Increment the attemptCount by 1
-    let maxAttempt = 0;
-    for await (let result of user.testResult) {
-      if (req.courseId == result.courseId) {
-        if(maxAttempt <result.attemptCount){
-          maxAttempt=result.attemptCount
-        }
+  }
+
+  const user = await User.findById(req.user._id);
+  // Increment the attemptCount by 1
+  let maxAttempt = 0;
+  for await (let result of user.testResult) {
+    if (req.courseId == result.courseId) {
+      if (maxAttempt < result.attemptCount) {
+        maxAttempt = result.attemptCount;
       }
     }
-    maxAttempt=maxAttempt+1;
-    // Update the testResult field with the provided data and incremented attemptCount
-    user.testResult.push({
-      attemptCount:maxAttempt,
-      attemptAt: new Date(),
-      courseId: req.courseId?new mongoose.Types.ObjectId(req.courseId):"",
-      courseName: courseDetail.name,
-      maxMark: courseDetail.maxMark,
-      cuttOffScore: courseDetail.cuttOffScore,
-      obtainedScore,
-      status:courseDetail.cuttOffScore<=obtainedScore?"passed":"failed"
-    });
-    user.testActivity={}
+  }
+  maxAttempt = maxAttempt + 1;
+  // Update the testResult field with the provided data and incremented attemptCount
+  user.testResult.push({
+    attemptCount: maxAttempt,
+    attemptAt: new Date(),
+    courseId: req.courseId ? new mongoose.Types.ObjectId(req.courseId) : "",
+    courseName: courseDetail.name,
+    maxMark: courseDetail.maxMark,
+    cuttOffScore: courseDetail.cuttOffScore,
+    obtainedScore,
+    status: courseDetail.cuttOffScore <= obtainedScore ? "passed" : "failed",
+  });
+  user.testActivity = {};
+
+  // // // Save the updated user
+  await user.save();
+  callBack(user.testResult);
+};
+
+function getUniqueResultsByAttemptCount(courseResults) {
+  // Create an object to store the highest attempt count for each course
+  const highestAttempts = {};
+
+  // Iterate through the course results array
+  courseResults.forEach((result) => {
+    const courseId = result.courseId;
+
+    // If the course is not in the highestAttempts object or the current attempt count is higher
+    if (!highestAttempts[courseId] || result.attemptCount > highestAttempts[courseId]) {
+      highestAttempts[courseId] = result.attemptCount;
+    }
+  });
+
+  // Filter the course results array to include only the records with the highest attempt count for each course
+  const uniqueResults = courseResults.filter((result) => {
+    return result.attemptCount === highestAttempts[result.courseId];
+  });
+
+  return uniqueResults;
+}
+
+
+exports.getResult = asyncHandler(async (req, res, next) => {
+  const user = await User.findOne({_id:req.user._id}).select("testResult")
+  // const courseResults = [
+  //   // ... (your course results array here)
+  // ];
   
-    // // // Save the updated user
-    await user.save();
-    callBack(user.testResult);
-  };
+  const uniqueResults = getUniqueResultsByAttemptCount(user.testResult);
+  return res.json(uniqueResults);
+
+
+});
--- routes/student.js
+++ routes/student.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const { protect } = require('../middleware/auth');
 const { getCourses, startTest, saveResult, updateActivity } = require('../controller/student/course');
+const { getResult } = require('../controller/student/result/result');
 
 // centre
 router.get('/course',protect, getCourses);
@@ -10,4 +11,5 @@ router.get('/start-test/:courseId',protect, startTest);
 router.post('/result/',protect, saveResult);
 
 router.put('/test-activity/',protect, updateActivity);
+router.get('/get-result',protect, getResult);
 module.exports = router;
